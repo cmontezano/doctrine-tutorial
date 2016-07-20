6d78216c3771cb34c85e92173831b2a74cd8cbbc
Carlos Montezano <cmontezanol@gmail.com>
2016-07-19 22:29:53 -0300

    add bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once 'vendor/autoload.php';
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/src"), $idDevMode);
+
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+$entityManager = EntityManager::create($conn, $config);
